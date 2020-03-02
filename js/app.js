a67babfeb59c1960c5c1208c58d12055d4218e2f
Marie Antons <maantons@mail.fhsu.edu>
2020-03-02 11:48:38 -0600

    added js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
 'use strict'
 console.log('Pet adoption project is in the works');
 
+var petOne = {
+    name: 'Fluffy',
+    breed: 'Maine Coon',
+    age: 0,
+    imageName: ,
+    interests: ['sunlight', 'mouse toys', 'cat nip' ],
+    isGoodWithKids: false,
+    isGoodWithDogs: true,
+    isGoodWithCats: false,
+    setAge: function(){
+        this.age = randomAge(3,12) + 'Months';
+    }
+}
+
+var petTwo = {
+    name: 'Tiger',
+    breed: 'Tiger',
+    age: 0,
+    imageName: ,
+    interests: ['rainy days', 'running', 'yarn' ],
+    isGoodWithKids: true,
+    isGoodWithDogs: true,
+    isGoodWithCats: true,
+    setAge: function(){
+        this.age = randomAge(3,12) + 'Months';
+    }
+}
+
+function randomAge(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+petOne.isFixed = true;
+petTwo.isFixed = true;
+
+
+var parentElement = document.getElementById(kittenProfiles) {
+
+}
+
+
+//create DOM element and render it in html
+
+//create a new elelement the parent element of the child element that we will create to render the pet article in the HTML
+
+//create an article
+
+//create an h2
+
+//create a paragraph
+
+//create a ul
+
+//create li
+
+//fill in li list
+
+//add image
+
+//set values on image to display correctly
+
+// 
